da3a2d172c0c50a9d54280a968f714579eda31e2
zohre265 <zorama70@gmail.com>
2025-08-06 02:05:44 +0330

    Update MainActivity.java

--- android/app/MainActivity_java/MainActivity.java
+++ android/app/MainActivity_java/MainActivity.java
@@ -3,5 +3,4 @@ package com.example.rama;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    // دیگر نیازی به onCreate() نیست مگر برای تنظیمات خاص.
 }
